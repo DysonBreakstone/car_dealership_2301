67b5a5f5928d0c993df9ab971fcaf3e16c3507a6
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-08 10:22:08 -0700

    add a spec file testing for average_price_of_car, cars_sorted_by_price, and inventory_hash

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -110,3 +110,47 @@ RSpec.describe "iteration 3" do
   end
 
 end
+
+RSpec.describe "Iteration 4" do
+  it "can relay average price of inventory in the form of a string" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+
+    expect(dealership.average_price_of_car).to eq("39,000")
+  end
+
+  xit "can sort cars by price" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+
+    p dealership.cars_sorted_by_price
+  end
+
+  xit "has an inventory hash" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+
+    p dealership.inventory_hash
+  end
+end
