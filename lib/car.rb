e12e160f748e23128875bbf906ef49badb4cb882
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-08 09:35:05 -0700

    add color attribute and paint! method

--- lib/car.rb
+++ lib/car.rb
@@ -1,6 +1,6 @@
 class Car
 
-    attr_reader :make, :model, :monthly_payment, :loan_length, :total_cost
+    attr_reader :make, :model, :monthly_payment, :loan_length, :total_cost, :color
 
   def initialize(make_model, monthly_payment, loan_length)
     make_and_model = make_model.split(" ")
@@ -9,7 +9,11 @@ class Car
     @monthly_payment = monthly_payment
     @loan_length = loan_length
     @total_cost = monthly_payment * loan_length
+    @color = nil
   end
 
+  def paint!(color)
+    @color = color
+  end
 
 end
